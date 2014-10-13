76511309d4fb5f47a396f19bf65696564bdb850e
Jun Huang <junhuang@Juns-MacBook-Air.local>
2014-10-13 14:50:49 -0400

    minor fix

--- SATSolverApp.cpp
+++ SATSolverApp.cpp
@@ -1,3 +1,4 @@
+#include "mpi.h"
 #include <iostream>
 #include <stdio.h>
 #include <memory>
@@ -8,7 +9,7 @@
 #include "Constants.h"
 #include "dpll.h"
 
-#include "mpi.h"
+
 
 
 typedef enum eAlgo{eGA=0,eDPLL} eAlgo;
